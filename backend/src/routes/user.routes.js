3fce874afe01047af547238db80e553d3cfda6b3
Official-Vinit <officialvinit1176@gmail.com>
2026-08-06 20:55:14 +0530

    add: user register route and controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,18 @@
 import {Router} from "express"
 import {registerUser} from "../controllers/user.controller.js"
+import {upload} from "../middleware/multer.js"
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(upload.fields([
+    {
+        name:"Avatar",
+        maxCount:1
+    },
+    {
+        name: "coverImage",
+        maxCount:1
+    }
+]),registerUser)
 // router.route("/login").post(loginUser)
 
 export default router;
